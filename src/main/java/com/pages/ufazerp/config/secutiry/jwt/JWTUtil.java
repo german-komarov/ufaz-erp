3dad19d045076fd3dec94e10d335a95783454ee4
german.komarov <german.komarov@ufaz.az>
2022-05-11 09:53:47 +0400

    Initial Domain structure

--- src/main/java/com/pages/ufazerp/config/secutiry/jwt/JWTUtil.java
+++ src/main/java/com/pages/ufazerp/config/secutiry/jwt/JWTUtil.java
@@ -6,15 +6,10 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JWTUtil {
