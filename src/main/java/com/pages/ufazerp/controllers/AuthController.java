8efcf988a4e710aa441b50fba581523d95ef4dd3
german.komarov <german.komarov@ufaz.az>
2022-05-26 12:46:53 +0400

    Refactoring;

--- src/main/java/com/pages/ufazerp/controllers/AuthController.java
+++ src/main/java/com/pages/ufazerp/controllers/AuthController.java
@@ -1,9 +1,14 @@
 package com.pages.ufazerp.controllers;
 
 import com.pages.ufazerp.config.secutiry.jwt.JWTUtil;
+import com.pages.ufazerp.domain.Student;
+import com.pages.ufazerp.domain.Teacher;
 import com.pages.ufazerp.domain.User;
 import com.pages.ufazerp.util.dto.LoginCredentials;
+import com.pages.ufazerp.util.dto.subject.GetSubjectDto;
 import com.pages.ufazerp.util.dto.users.GetUserDto;
+import com.pages.ufazerp.util.dto.users.student.GetStudentDto;
+import com.pages.ufazerp.util.dto.users.teacher.GetTeacherDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -43,7 +48,13 @@ public class AuthController {
             String token = jwtUtil.generateToken(body.getEmail());
             Map<String, Object> json = new HashMap<>();
             json.put("jwt", token);
-            json.put("user", new GetUserDto(user));
+            if(user instanceof Student) {
+                json.put("user", new GetStudentDto((Student)user));
+            } else if(user instanceof Teacher) {
+                json.put("user", new GetTeacherDto((Teacher) user));
+            } else {
+                json.put("user", new GetUserDto(user));
+            }
             return ok(json);
         } catch (AuthenticationException e){
             return status(HttpStatus.UNAUTHORIZED).body(message("Wrong credentials"));
